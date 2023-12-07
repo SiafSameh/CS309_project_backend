d154322aaea49954639e162f41895541e8c1d21a
seif sameh <152812697+SiafSameh@users.noreply.github.com>
2023-12-07 19:57:09 +0200

    set mongoodb

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
 const express = require("express")
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt');
+
+const mongouri = "mongodb://localhost:27017/data"
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+
+
+mongoose.set("strictQuery", false)
+mongoose
+.connect('mongodb://127.0.0.1:27017/data')
+.then(() => {
+    console.log('connected to MongoDB')
+    //listen on specific port 
+    app.listen(3000, () => console.log('app started on port 3000'))
+}).catch((error) => {
+    console.log('cant connect to mongodb'+error)
+})
