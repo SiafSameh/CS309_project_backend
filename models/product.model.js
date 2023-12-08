08d2bc3f86a1c21fd6c8ca76eea6c05a462c9883
Youssef Ashraf <142300609+youssef604@users.noreply.github.com>
2023-12-08 17:35:11 +0200

    add models

---
+++ models/product.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+ProductSchema = new Schema
+(   {
+        name: { type: String, required: [true, "Product name must be included"] },
+        price: { type: Number, required: [true, "Please provide price value"] },
+        description: {type: String, required: false },
+        image: { type: String, required: true  },
+        rating: {type: Number, default: 0,required: false},
+        // numOfReviews: {    type: Number, default: 0, required: true },
+        // user: {
+        //     type: mongoose.Types.ObjectId,
+        //     ref: "User",
+        //     required: true,
+        //   },
+
+    },
+    {
+        timestamps: true
+    }
+);
+  const ProductModel = mongoose.model('Product', ProductSchema);
+  module.exports = mongoose.model('product', ProductSchema);
---
+++ models/user.model.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
+// const validator = require('validator')
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+    {
+        fullName: { type: String, required: [true, "please enter your name"] ,minlentgh: 3,maxlengh:30,uniqe: true},
+        email: { type: String, unique: true, required: [true ,"please provide email"],
+            match: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$/,
+            //  validate( value ) {
+            //     if( !validator.isEmail( value )) {
+            //         throw new Error( 'Email is invalid' )
+            //     } }
+            },
+
+        password: { type: String, required: true },
+        phoneNumber: { type: String, required: false },
+        image: { type: String, required: false },
+        bio: {type: String, required: false},
+        isAdmin: {type: Boolean, default: false }
+
+    },
+    {
+        timestamps: true
+    }
+ 
+
+);
+//Hash plain password before saving
+userSchema.pre('save', async function(next) {
+    const user = this
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 10)
+    }
+
+    next()
+})
+
+
+
+
+
+const user= mongoose.model("User",userSchema)
+
+module.exports = mongoose.model('User', userSchema);
